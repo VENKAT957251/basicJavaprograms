599cd77b64756b0104188daaebbc41953d92e1bc
saipavan <saipavan2217@gmail.com>
2021-04-30 12:37:46 +0530

    commiting from another workspace

--- BasicJavaprg/src/practiceJavaprgs/arrayTOarraylist.java
+++ BasicJavaprg/src/practiceJavaprgs/arrayTOarraylist.java
@@ -9,7 +9,7 @@ public class arrayTOarraylist {
 	public static void main(String[] args) {
 		// convering array to arralist
 //https://www.javatpoint.com/testng-annotation-attributes
-		String[] arr = {"Venkat","sai","pavan","mahesh"};
+		String[] arr = {"Venkat","sai","Venkatasaipavamn","pavan"};
 //		System.out.println(Arrays.toString(arr));
 		//https://www.geeksforgeeks.org/conversion-of-array-to-arraylist-in-java/
 		//Exception in thread "main" java.lang.UnsupportedOperationException
@@ -17,6 +17,7 @@ public class arrayTOarraylist {
 		List<String> lst = new ArrayList<String>(Arrays.asList(arr));
 		System.out.println(lst);
 		lst.add("mahesh");
+		lst.add("patturaj");
 		System.out.println(lst);
 
 		
